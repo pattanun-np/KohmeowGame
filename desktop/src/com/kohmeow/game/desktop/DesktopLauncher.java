794e5989b034f1c7773910b56d8c5e1c572d8328
PattananNP <phattanun19army@gmail.com>
2022-04-14 12:07:36 +0700

    Init

---
+++ desktop/src/com/kohmeow/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kohmeow.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kohmeow.game.KohMeowGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KohMeowGame(), config);
+	}
+}
