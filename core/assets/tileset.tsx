5aa85c864335c7f8667eba16d6ca2e74814e819b
PattananNP <phattanun19@hotmail.com>
2022-04-22 21:43:50 +0700

    Feat: Catwalking

---
+++ core/assets/tileset.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="tileset" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Atlas/tileset.png" trans="ff00ff" width="1024" height="1024"/>
+ <wangsets>
+  <wangset name="Dirts" type="corner" tile="109">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="65" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="66" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="67" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="76" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="77" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="78" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="97" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="99" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="108" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="109" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="110" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="129" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="130" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="131" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="140" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="141" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="142" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
